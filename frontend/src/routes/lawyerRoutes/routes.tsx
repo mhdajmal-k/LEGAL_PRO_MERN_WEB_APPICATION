3e7b44850b7637859ef776b133f332c2d061da79
Muhammed Ajmal k <ajmalchundappuram@gmail.com>
2024-09-30 18:28:41 +0530

    some changed done in Professtional data in laywer

--- frontend/src/routes/lawyerRoutes/routes.tsx
+++ frontend/src/routes/lawyerRoutes/routes.tsx
@@ -2,12 +2,14 @@ import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import LawyerSingUp from '../../pages/lawyerpages/LawyerSingUP'
 import LawyerVerifyOtp from '../../pages/lawyerpages/VerifyOtp'
+import ProfessionalDetails from '../../pages/lawyerpages/ProfessionalDetails'
 
 const LawyerRoutes: React.FC = () => {
     return (
         <Routes>
             <Route path='/signup' element={<LawyerSingUp />} />
             <Route path='/verify-otp' element={<LawyerVerifyOtp />} />
+            <Route path='/ProfessionalData' element={<ProfessionalDetails />} />
         </Routes>
     )
 }
