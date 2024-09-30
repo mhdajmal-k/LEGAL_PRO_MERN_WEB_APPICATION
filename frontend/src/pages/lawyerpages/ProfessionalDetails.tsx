14a1ba1075042bc13041d28a431c754f45e207e0
Muhammed Ajmal k <ajmalchundappuram@gmail.com>
2024-09-30 18:28:41 +0530

    some changed done in Professtional data in laywer

---
+++ frontend/src/pages/lawyerpages/ProfessionalDetails.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Navbar from "../../layout/Navbar";
+import LegalFooter from "../../layout/footer";
+
+const ProfessionalDetails: React.FC = () => {
+    return <>
+        <Navbar />
+        <h1>hi</h1>
+        <LegalFooter />
+    </>
+}
+
+export default ProfessionalDetails
