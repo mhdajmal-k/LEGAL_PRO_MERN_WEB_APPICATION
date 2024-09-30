5b7bd359931fd5e5e5c79b3ed9f2ee0a1f4ad95d
Muhammed Ajmal k <ajmalchundappuram@gmail.com>
2024-09-30 17:57:22 +0530

    verifyOtp done

--- backend/src/application/useCases/lawyer/lawyerAuthUseCase.ts
+++ backend/src/application/useCases/lawyer/lawyerAuthUseCase.ts
@@ -6,7 +6,10 @@ import { iJwtService } from "../../../domain/services/ijwtService";
 import { iOTPService } from "../../../domain/services/iOTPService";
 import { S3Service } from "../../../frameWorks/config/s3Setup";
 import { CustomError } from "../../../frameWorks/middleware/errorHandiler";
-import { generatingSignUpToken } from "../../../frameWorks/utils/jwt";
+import {
+  decodeSingUpToken,
+  generatingSignUpToken,
+} from "../../../frameWorks/utils/jwt";
 class LawyerAuthInteractor implements ILawyerAuthInteractor {
   constructor(
     private readonly Repository: iLawyerRepository,
@@ -21,20 +24,20 @@ class LawyerAuthInteractor implements ILawyerAuthInteractor {
   ): Promise<{ statusCode: number; message: string; result: string | {} }> {
     try {
       const { email, userName, password } = data;
-      const userExists = await this.Repository.lawyerAlreadyExist(email);
-      if (userExists) {
+      const LawyerExists = await this.Repository.lawyerAlreadyExist(email);
+      if (LawyerExists) {
         return { statusCode: 409, message: "User Already Exists", result: {} };
       }
-
+      // let uploadPromise: Promise<string> | undefined;
       if (file) {
         const key = `lawyer-profiles/${Date.now()}-${file.originalname}`;
-        await this.s3Service.uploadFile(file, key);
+        const uploadPromise = this.s3Service.uploadFile(file, key);
         data.profile_picture = key;
       }
       const OTP = this.optGenerator.generateOTP();
       this.nodeMailer.sendOTP(email, OTP, userName);
-      const SingUPTempToken = this.jwt.generateToken(data, OTP);
-
+      const SingUPTempToken = generatingSignUpToken(data, OTP);
+      // await Promise.all([uploadPromise, emailPromise]);
       return {
         statusCode: 200,
         message: "OTP sent successfully",
@@ -44,7 +47,6 @@ class LawyerAuthInteractor implements ILawyerAuthInteractor {
       throw error;
     }
   }
-
   async verifyOtp(
     otp: string,
     token: string
@@ -53,7 +55,47 @@ class LawyerAuthInteractor implements ILawyerAuthInteractor {
     message: string;
     result?: {} | ILawyer | undefined;
   }> {
-    return { statusCode: 200, message: "success", result: {} };
+    try {
+      const decodeToken = decodeSingUpToken(token);
+      console.log(decodeToken);
+
+      const currentTime = Math.floor(Date.now() / 1000);
+      if (decodeToken.otpExpiresAt < currentTime)
+        return {
+          statusCode: 401,
+          message: "OTP has expired",
+          result: undefined,
+        };
+
+      if (otp !== decodeToken.OTP)
+        return {
+          statusCode: 401,
+          message: "entered Invalid OTP",
+          result: undefined,
+        };
+      const creatingNewLawyer = await this.Repository.createLawyer(
+        decodeToken.user
+      );
+      if (!creatingNewLawyer) {
+        const error = new Error();
+        error.message = "Failed to create new user";
+        throw error;
+      }
+
+      const jwtToken = this.jwt.generateToken(creatingNewLawyer._id, "lawyer");
+      const { password, ...userDataWithoutPassword } =
+        creatingNewLawyer.toObject();
+      return {
+        statusCode: 201,
+        message: "OTP verified successfully",
+        result: {
+          user: userDataWithoutPassword,
+          tokenJwt: jwtToken,
+        },
+      };
+    } catch (error) {
+      throw error;
+    }
   }
 }
 
