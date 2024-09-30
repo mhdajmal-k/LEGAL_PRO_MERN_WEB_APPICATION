37301d3d19148397d3877167c96ae6cc6479c8fa
Muhammed Ajmal k <ajmalchundappuram@gmail.com>
2024-09-30 17:57:22 +0530

    verifyOtp done

--- backend/src/interFace_adapters/controlers/lawyer/lawyerAuthController.ts
+++ backend/src/interFace_adapters/controlers/lawyer/lawyerAuthController.ts
@@ -1,6 +1,7 @@
 import { Request, Response, NextFunction } from "express";
 import ILawyerAuthInteractor from "../../../domain/entites/iuseCase/iLawyerAuth";
 import { validateLawyerInput } from "../../../frameWorks/utils/helpers/validationHelpers";
+import { ILawyer } from "../../../domain/entites/imodels/iLawyer";
 class LawyerAuthController {
   constructor(private lawyerAuthInteractor: ILawyerAuthInteractor) {}
   async lawyerSignUp(
@@ -20,25 +21,72 @@ class LawyerAuthController {
         return;
       }
       if (!file) {
-        res
-          .status(400)
-          .json({
-            status: false,
-            message: "profile image is required",
-            result: {},
-          });
+        res.status(400).json({
+          status: false,
+          message: "profile image is required",
+          result: {},
+        });
         return;
       }
 
       const response = await this.lawyerAuthInteractor.lawyerSingUp(data, file);
 
-      res.status(response.statusCode).json({
+      res.cookie("lawyerAuth_token", response.result, {
+        httpOnly: true,
+        sameSite: "strict",
+        maxAge: 5 * 60 * 1000,
+      });
+
+      res.status(200).json({
         status: true,
         message: response.message,
-        result: response.result,
+        result: {},
       });
     } catch (error) {
       next(error);
+    }
+  }
+  async lawyerVerifyOtp(req: Request, res: Response): Promise<any> {
+    try {
+      const { otp } = req.body;
+
+      if (!otp || otp.trim() == "")
+        return res
+          .status(400)
+          .json({ success: false, message: "otp is required", result: {} });
+      const token = req.cookies.lawyerAuth_token;
+      console.log(token);
+      if (!token)
+        return res.status(400).json({
+          success: false,
+          message: "session is expired try again",
+          result: {},
+        });
+      const response = await this.lawyerAuthInteractor.verifyOtp(otp, token);
+
+      const { statusCode, message, result } = response;
+      if (result) {
+        const data = result as ILawyer;
+        res.cookie("auth_lawyerAccessToken", data.token, {
+          httpOnly: true,
+          sameSite: "strict",
+          maxAge: 5 * 60 * 1000,
+        });
+        res.clearCookie("lawyerAuth_token");
+        return res.status(statusCode).json({
+          status: true,
+          message: message,
+          result: result,
+        });
+      } else {
+        res.status(400).json({
+          status: false,
+          message: response.message,
+          result: {},
+        });
+      }
+    } catch (error) {
+      console.log(error);
     }
   }
 }
--- backend/src/interFace_adapters/repositories/lawyerRepositories/lawyerAuthRepository.ts
+++ backend/src/interFace_adapters/repositories/lawyerRepositories/lawyerAuthRepository.ts
@@ -1,10 +1,33 @@
 import { ILawyer } from "../../../domain/entites/imodels/iLawyer";
-import { IUser } from "../../../domain/entites/imodels/Iuser";
 import iLawyerRepository from "../../../domain/entites/irepositories/ilawyerRepositories";
 import Lawyer from "../../../frameWorks/database/models/lawyerModel";
+import { hashPassword } from "../../../frameWorks/utils/helpers/passwordHelper";
 
 class LawyerAuthRepository implements iLawyerRepository {
-  async createLawyer(data: ILawyer): Promise<any> {}
+  async createLawyer(data: ILawyer): Promise<any> {
+    try {
+      const hashedPassword = hashPassword(data.password);
+      const newUser = new Lawyer({
+        userName: data.userName,
+        email: data.email,
+        password: hashedPassword,
+        city: data.city,
+        gender: data.gender,
+        zipcode: data.zipcode,
+        state: data.state,
+        profile_picture: data.profile_picture,
+      });
+      try {
+        await newUser.save();
+        return newUser;
+      } catch (error) {
+        throw error;
+      }
+    } catch (error) {
+      console.log(error);
+      throw error;
+    }
+  }
   async validLayer(email: string): Promise<any> {
     try {
       const user = await Lawyer.findOne({ email: email }).lean();
@@ -15,12 +38,13 @@ class LawyerAuthRepository implements iLawyerRepository {
       }
     } catch (error) {
       console.log(error);
+      throw error;
     }
   }
 
   async lawyerAlreadyExist(email: string): Promise<boolean> {
-    const user = await Lawyer.findOne({ email: email }).lean();
-    return !!user;
+    const lawyer = await Lawyer.findOne({ email: email }).lean();
+    return !!lawyer;
   }
 }
 export default LawyerAuthRepository;
