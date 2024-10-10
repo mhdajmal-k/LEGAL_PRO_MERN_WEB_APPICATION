249f8e1c7f31188cfb9ae08289edfd2b86d9ef49
Muhammed Ajmal k <ajmalchundappuram@gmail.com>
2024-10-10 10:53:38 +0530

    route protected

--- frontend/src/routes/adminRoutes/routes.tsx
+++ frontend/src/routes/adminRoutes/routes.tsx
@@ -6,7 +6,7 @@ import AdminLoginForm from '../../pages/adminpages/AdminLogin';
 import UsersList from '../../pages/adminpages/UserList';
 import AdminLayout from '../../components/AdminComponents.tsx/AdminLayout';
 import { AdminDashBoard } from '../../pages/adminpages/adminDashBoard';
-import LawyerPublicRoute from '../AdminPublicRoute';
+import AdminPublicRoute from '../AdminPublicRoute';
 import AdminProtectRoute from '../AdminProtectRoute';
 import ApprovalLawyerList from '../../pages/adminpages/ApprovalLawyerList';
 import LawyerList from '../../pages/adminpages/LaywerList';
@@ -16,7 +16,7 @@ import LawyerList from '../../pages/adminpages/LaywerList';
 const AdminRoutes: React.FC = () => {
     return (
         <Routes>
-            <Route element={<LawyerPublicRoute />}>
+            <Route element={<AdminPublicRoute />}>
                 <Route path='/login' element={<AdminLoginForm />} />
             </Route>
             <Route element={<AdminProtectRoute />}>
