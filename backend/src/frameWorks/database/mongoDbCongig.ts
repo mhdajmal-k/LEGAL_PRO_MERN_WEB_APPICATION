eec9fc266190e1cb4a1a76aea206b4a191c65cff
Muhammed Ajmal k <ajmalchundappuram@gmail.com>
2024-11-24 14:39:52 +0530

    auth lawyer done error 2

--- backend/src/frameWorks/database/mongoDbCongig.ts
+++ backend/src/frameWorks/database/mongoDbCongig.ts
@@ -2,8 +2,8 @@ import mongoose from "mongoose";
 import { config } from "../config/envConfig";
 export const connectToDatabase = async () => {
   try {
-    await mongoose.connect(config.DB_URLLOCAL);
-    // await mongoose.connect(config.DB_URL);
+    // await mongoose.connect(config.DB_URLLOCAL);
+    await mongoose.connect(config.DB_URL);
     console.log("DB CONNECTED");
   } catch (error: unknown) {
     if (error instanceof Error) {
