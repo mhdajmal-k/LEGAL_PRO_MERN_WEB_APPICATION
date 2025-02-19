bca0ed6ed5a43478173d425fd71146c0ad325c65
Muhammed Ajmal k <ajmalchundappuram@gmail.com>
2025-02-19 12:22:42 +0530

    razorpay bug fixed

--- backend/src/frameWorks/services/razorPay.ts
+++ backend/src/frameWorks/services/razorPay.ts
@@ -1,5 +1,6 @@
 import Razorpay from "razorpay";
 
+console.log(process.env.RAZORPAY_ID)
 if (!process.env.RAZORPAY_ID || !process.env.RAZORPAY_SECRET) {
   throw new Error("Razorpay environment variables are not set!");
 }
